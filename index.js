147405b50ec5aa6349c3cf87a83b88ce55dd2637
Muhammad faizan <shahidfaiz31@gmail.com>
2024-01-25 21:41:10 +0500

    i am making a game

---
+++ index.js
@@ -0,0 +1,9 @@
+const pong = document.querySelector("#pong");
+const ctx = pong.getcontext("2d");
+const score = document.querySelector("#score");
+const resetbtn = document.querySelector("#resetbtn");
+
+const gameWidth = field.width;
+const gameHeight = field.height;
+const 
+
