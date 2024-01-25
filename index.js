eec6db2d8b0a4e63b85e6e7cca4fc53b245f973d
Muhammad faizan <shahidfaiz31@gmail.com>
2024-01-25 23:08:39 +0500

    game still can't run

--- index.js
+++ index.js
@@ -13,5 +13,29 @@ const ballColor = "red";
 const ballborderColor = "black";
 const ballRadius = 12.5;
 const paddleSpeed =50;
- 
+
+let  intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth/2;
+let ballY = gameHeight/2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+
+let paddle1 = {
+    width: 24,
+    height: 100,
+    x: 0,
+    y: 0
+}
+let paddle2 = {
+    width: 24,
+    height: 100,
+    x: gameWidth -24,
+    y: gameHeight - 100
+}
+window.addEventListener("keydown", changeDirectio);
+resetbtn 
+
 
