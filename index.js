22f3c7d332ef6f6aee1cc20fc240cb2f465c7081
Muhammad faizan <shahidfaiz31@gmail.com>
2024-01-25 22:16:02 +0500

    playing abilities added

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const resetbtn = document.querySelector("#resetbtn");
 
 const gameWidth = field.width;
 const gameHeight = field.height;
-const 
+const boardbackground = "forestgreen";
+const paddle1Color = "black";
+const paddle2Color = "white";
+const paddleBorder = "yellow";
+const ballColor = "red";
+const ballborderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed =50;
+ 
 
